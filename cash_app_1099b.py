f3290a4563ea5dc0ced30f12ba2638f857c725f4
Paul Donnelly <donnelpa@gmail.com>
2025-03-01 12:54:41 -0600

    fix remaining xpaths, speed-up approach

--- cash_app_1099b.py
+++ cash_app_1099b.py
@@ -33,7 +33,7 @@ driver.find_element(By.XPATH, '//label[div[text()="Spreadsheet entry"]]').click(
 driver.find_element(By.XPATH, '//button[contains(., "Continue")]').click()
 
 
-def import_trades(path: Path):
+def import_trades(path: Path) -> pl.DataFrame:
     trades = pl.read_csv(path)
 
     trades = trades.rename(
@@ -48,6 +48,12 @@ def import_trades(path: Path):
         },
         strict=True,
     ).with_columns(pl.col("Covered").replace("Uncovered", "Not Covered"))
+
+    excl_trades = trades.filter(pl.col("Code").eq("X"))
+    if not excl_trades.is_empty():
+        print("Excluded trades:")
+        print(excl_trades.select("Description", "Cost"))
+        trades = trades.filter(pl.col("Code").ne("X"))
 
     # Define the required columns
     required_columns = [
@@ -68,9 +74,9 @@ def import_trades(path: Path):
     return trades
 
 
-def input_trades(df):
+def input_trades(df: pl.DataFrame) -> None:
     nrow_need = len(df)
-    table = driver.find_element(By.ID, "capitalGainsTable")
+    table = driver.find_element(By.XPATH, '//table[@role="table"]')
     # increase rows as needed
     while True:
         # determine the number of rows the current table has
@@ -80,7 +86,9 @@ def input_trades(df):
             break
         # Find the addRows button to scroll to
         # it's not clickable unless it's in view
-        addRows = driver.find_element(By.ID, "addRows")
+        addRows = driver.find_element(
+            By.XPATH, '//button[normalize-space()="Add Rows"]'
+        )
         # Scroll to addRows and click
         driver.execute_script("arguments[0].scrollIntoView();", addRows)
         addRows.click()
@@ -88,39 +96,36 @@ def input_trades(df):
     i = 0
     for row in df.iter_rows(named=True):
         # input the sale category
-        box = row["Code"]
-        trade_type = row["Type"].title()
-        is_covered = row["Covered"].lower()
-
         el = Select(
             driver.find_element(By.NAME, f"capitalGains[{i}].reportingCategory")
         )
-        el.select_by_visible_text(f"Box {box} - {trade_type} {is_covered}")
-
-        # enter the description
-        desc = row["Description"]
-        driver.find_element(By.NAME, f"capitalGains[{i}].description").clear()
-        driver.find_element(By.NAME, f"capitalGains[{i}].description").send_keys(desc)
-
-        # enter acq and sell dates
-        acq = row["DtAcq"]
-        sold = row["DtSold"]
-        driver.find_element(By.NAME, f"capitalGains[{i}].dateAcquired").clear()
-        driver.find_element(By.NAME, f"capitalGains[{i}].dateSold").clear()
-        driver.find_element(By.NAME, f"capitalGains[{i}].dateAcquired").send_keys(acq)
-        driver.find_element(By.NAME, f"capitalGains[{i}].dateSold").send_keys(sold)
-
-        # enter proceeds
-        proceeds = row["Proceeds"]
-        driver.find_element(By.NAME, f"capitalGains[{i}].salesPrice").clear()
-        driver.find_element(By.NAME, f"capitalGains[{i}].salesPrice").send_keys(
-            proceeds
+        el.select_by_visible_text(
+            f"Box {row['Code']} - {row['Type'].title()} {row['Covered'].lower()}"
         )
 
-        # enter cost basis
-        cost = row["Cost"]
-        driver.find_element(By.NAME, f"capitalGains[{i}].cost").clear()
-        driver.find_element(By.NAME, f"capitalGains[{i}].cost").send_keys(cost)
+        # enter the description
+        desc = driver.find_element(By.NAME, f"capitalGains[{i}].description")
+        desc.clear()
+        desc.send_keys(row["Description"])
+
+        # enter acq and sell dates
+        dtaq = driver.find_element(By.NAME, f"capitalGains[{i}].dateAcquired")
+        dtaq.clear()
+        dtaq.send_keys(row["DtAcq"])
+
+        dtsl = driver.find_element(By.NAME, f"capitalGains[{i}].dateSold")
+        dtsl.clear()
+        dtsl.send_keys(row["DtSold"])
+
+        # # enter proceeds
+        proceeds = driver.find_element(By.NAME, f"capitalGains[{i}].salesPrice")
+        proceeds.clear()
+        proceeds.send_keys(row["Proceeds"])
+
+        # # enter cost basis
+        cost = driver.find_element(By.NAME, f"capitalGains[{i}].cost")
+        cost.clear()
+        cost.send_keys(row["Cost"])
 
         i += 1
 
@@ -135,3 +140,4 @@ trades.describe()
 input_trades(trades)
 
 trades.select(pl.col("Proceeds") - pl.col("Cost")).sum()
+trades.select(pl.col("Proceeds").round() - pl.col("Cost").round()).sum()
